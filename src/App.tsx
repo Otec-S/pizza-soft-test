3150b16c9569978d7311d867926fea891f4b0182
Sergey Grigorash <adviser@bk.ru>
2024-09-09 13:10:25 +0500

    feat сделал отдельный layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import EmployeesList from "./pages/employees-list/employees-list";
 import EmployeeEdit from "./pages/employee-edit/employee-edit";
 import EmployeeAdd from "./pages/employee-add/employee-add";
 import NotFound from "./pages/not-found/not-found";
+import LayoutGeneral from "./components/layout/layout";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<EmployeesList />} />
+        {/* FIXME: */}
+        <Route path="/layout" element={<LayoutGeneral />} />
         <Route path="/employee-edit/:id" element={<EmployeeEdit />} />
         <Route path="/employee-add" element={<EmployeeAdd />} />
         <Route path="/404" element={<NotFound />} />
